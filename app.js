b323b7bee4c6150870dd1940f8bec24d2e74f977
Deepika Chouhan <dipikachouhan@Dipikas-MacBook-Air.local>
2026-07-17 18:02:42 +0530

    feat: implement user registration with bcrypt password hashing

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import userRoutes from './routes/userRoutes.js';
+import authRoutes from './routes/authRoutes.js';
 import {logger} from './middleware/logger.js';
 import {requestCounter} from './middleware/requestCounter.js';
 
@@ -11,6 +12,8 @@ app.use(requestCounter); // global request counter middleware for all routes
 app.use(logger); // global logger middleware for all routes
 
 app.use('/users', userRoutes);
+app.use('/auth', authRoutes);
+
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
--- middleware/logger.js
+++ middleware/logger.js
@@ -1,9 +1,17 @@
 export function logger(req, res, next) {
-    const start = Date.now();
-    res.on('finish', () => {
-        const duration = Date.now() - start;
-        console.log(`${req.method} ${req.originalUrl} ${res.statusCode} - ${duration}ms`);
+    const startTime = Date.now();
+
+    const timestamp = new Date().toISOString();
+
+    console.log(`[${timestamp}] ${req.method} ${req.originalUrl} - Request Received`);
+
+    res.on("finish", () => {
+        const duration = Date.now() - startTime;
+
+        console.log(
+            `[${timestamp}] ${req.method} ${req.originalUrl} | ${res.statusCode} | ${duration}ms | ${req.ip}`
+        );
     });
-     console.log(`${req.method} ${req.originalUrl}`);
+
     next();
 }
